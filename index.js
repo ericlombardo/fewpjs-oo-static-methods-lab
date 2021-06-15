0657b922286e36ec9a0cc0e1af9c1e7accdad6e9
Eric <eric.m.lombardo@gmail.com>
2021-06-15 15:15:39 -0400

    commit tree e783404b9f407bd6d73d98f317a513f4040354b3

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+  static sanitize(string) { 
+    return string.replace(/[^a-zA-Z0-9-' ]/g, '') // g makes it global replace, not just first
+  }
+
+  static titleize(string) {
+    const excludedWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let newString = string.split(' ').map((word, index) => {
+      if (index === 0 || !excludedWords.includes(word)) {
+        return word.charAt(0).toUpperCase() + word.slice(1)
+      } else {
+        return word
+      }
+    })
+    return newString.join(' ')
+  }
 }
